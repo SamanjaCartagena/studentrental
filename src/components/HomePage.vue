9eba5c903367eaeff9f7fe33c196e270712c2636
SamanjaCartagena <cart.samanja09@gmail.com>
2023-06-20 00:13:28 -0400

    header from bulma

---
+++ src/components/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <the-header></the-header>
+</template>
+
+<script>
+import TheHeader from '../views/TheHeader.vue'
+
+export default {
+    components:{
+        TheHeader
+    }
+
+}
+</script>
+
+<style>
+
+</style>
