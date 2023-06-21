ba4c4b2a542fc5239a0a85c69bb1f32e735bfc72
Samanja Cartagena <c.samanja09@Pabs.local>
2023-06-21 00:39:07 -0400

    admin page added

---
+++ src/components/AdminPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <the-header></the-header>
+</template>
+
+<script>
+import TheHeader from '../views/TheHeader.vue'
+
+export default {
+  components:{
+    TheHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/SecondPart.vue
+++ src/views/SecondPart.vue
@@ -4,7 +4,7 @@
   <div v-for="i in 12" :key="i" class="card">
   <div class="card-content">
     <p class="title">
-      “There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”
+      <img :src="image1"/> 
     </p>
     <p class="subtitle">
       Jeff Atwood
@@ -13,7 +13,7 @@
   <footer class="card-footer">
     <p class="card-footer-item">
       <span>
-        View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
+ <button @click="triggerModal(i)">View</button>
       </span>
     </p>
     <p class="card-footer-item">
@@ -28,12 +28,22 @@
 </template>
 
 <script>
+import image1 from '../assets/campus.jpg'
 export default {
-    
+    data(){
+        return{
+    image1:image1
+        }
+    },
+    methods:{
+        triggerModal(v){
+            alert(v+" has been triggered")
+        }
+    }
 }
 </script>
 
-<style scoped>
+<style >
 @import 'bulma/css/bulma.min.css';
 .card{
     width:22%;
